eec0a752b538c633570229a16360df4784851227
Steven Ng <stevenrng@gmail.com>
2018-06-14 15:40:34 -0400

    changed sign up form to bootstrap and used devise routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {:registrations => "registrations"}
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
   end
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  
   root to: 'blogs#index'
   resources :blogs
 end
