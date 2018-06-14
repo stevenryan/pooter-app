6f61928a6e52840891594014d5029daefada3537
Steven Ng <stevenrng@gmail.com>
2018-06-14 15:37:26 -0400

    changed registrations to include name attribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+  devise_for :users, :controllers => {:registrations => "registrations"}
   root to: 'blogs#index'
   resources :blogs
 end
