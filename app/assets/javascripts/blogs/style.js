3cc1ddc55b785e7aec421b20ede52d0ab7be16a0
Steven Ng <stevenrng@gmail.com>
2018-06-15 11:31:33 -0400

    changed post colors to be bathroom colors and added the modal functionality

--- app/assets/javascripts/blogs/style.js
+++ app/assets/javascripts/blogs/style.js
@@ -32,13 +32,13 @@ var mode = Math.floor(Math.random() * 2);
 
 // Randomly returns a color scheme
 function randomColorSet() {
-    var colorSet1 = ['#45CCFF', '#49E83E', '#FFD432', '#E84B30', '#B243FF'];
-    var colorSet2 = ['#FF6138', '#FFFF9D', '#BEEB9F', '#79BD8F', '#79BD8F'];
-    var colorSet3 = ['#FCFFF5', '#D1DBBD', '#91AA9D', '#3E606F', '#193441'];
-    var colorSet4 = ['#004358', '#1F8A70', '#BEDB39', '#FFE11A', '#FD7400'];
-    var colorSet5 = ['#105B63', '#FFFAD5', '#FFD34E', '#DB9E36', '#BD4932'];
-    var colorSet6 = ['#04BFBF', '#CAFCD8', '#F7E967', '#A9CF54', '#588F27'];
-    var colorSet7 = ['#405952', '#9C9B7A', '#FFD393', '#FF974F', '#F54F29'];
+    var colorSet1 = ['#666633', '#77773c', '#888844', '#99994d', '#aaaa55'];
+    var colorSet2 = ['#4d3319', '#604020', '#734d26', '#86592d', '#996633'];
+    var colorSet3 = ['#4d2600', '#663300', '#804000', '#994d00', '#b35900'];
+    var colorSet4 = ['#4d3300', '#664400', '#805500', '#996600', '#b37700'];
+    var colorSet5 = ['#ffdf80', '#ffe699', '#ffecb3', '#fff2cc', '#fff9e6'];
+    var colorSet6 = ['#ffff66', '#ffff80', '#ffff99', '#ffffb3', '#ffffcc'];
+    var colorSet7 = ['#ffe680', '#ffeb99', '#fff0b3', '#fff5cc', '#fffae6'];
     var randomSet = [colorSet1, colorSet2, colorSet3, colorSet4, colorSet5, colorSet6, colorSet7];
     return randomSet[Math.floor(Math.random() * randomSet.length )];
 }
