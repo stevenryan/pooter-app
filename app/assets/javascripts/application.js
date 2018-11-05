7555b14adaa96329b1a1328f185eb35c8b6f0ff3
Steven Ng <stevenrng@gmail.com>
2018-11-05 12:28:55 -0500

    ready my javascript please

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
+//= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require bootstrap-sprockets
